ffc5468b659f5f55bdf6a884a7adc6a98baf8bfe
vitalya834 <44960212+vitalya834@users.noreply.github.com>
2025-01-14 14:53:01 +0100

    Coding-Task: Use operators & data types

--- script.js
+++ script.js
@@ -8,3 +8,25 @@ console.log("Hello Welt".length);
 console.log("Hello Welt".toUpperCase());
 console.log("Hello Welt"[0]);
 console.log("Hello Welt"[4]);
+console.log(1==1);
+console.log(1!=1);
+console.log(1>1);
+console.log(1>=1);
+console.log(1<1);
+console.log(1<=1);
+console.log("V" + "i" + "t" + "a" + "l" + "i"  + "i");
+// Error! Make it work
+console.log("Hallo");
+// Error! Make it work
+console.log("That doesnt work");
+// should return true. Change only the operator to fix it.
+console.log(50 + 2 >= 52);
+// should be false. Change only the operator to fix it.
+console.log("333" === 333);
+// Wrong result: Expected 555 not 855. Change only the operator to fix it.
+console.log((600 + 510) / 2);
+// Wrong result: Expected a Number 4 received 22
+console.log(2 + 2);
+console.log(25 / 5); // Result: 5
+console.log(50 / 10); // Result: 5
+console.log(25 / 5=== 50/10); // Result: 5

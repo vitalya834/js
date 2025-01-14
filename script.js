4052ecc4a547609b82a2861df635ec98e097bc9c
vitalya834 <44960212+vitalya834@users.noreply.github.com>
2025-01-14 13:21:24 +0100

    feat: initial

--- script.js
+++ script.js
@@ -3,3 +3,8 @@
 wei  man sieht */
 
 console.log("Hello, World!");
+console.log("1+1"); 
+console.log("Hello Welt".length);
+console.log("Hello Welt".toUpperCase());
+console.log("Hello Welt"[0]);
+console.log("Hello Welt"[4]);
